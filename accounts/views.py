b56b20c2b76b8b2da20479f23d5a01a5cec66399
Brian Orandi <brianorandi97@gmail.com>
2025-07-06 01:28:25 +0300

    Added base.html and navbar partial

--- accounts/views.py
+++ accounts/views.py
@@ -8,3 +8,28 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CustomUserChangeForm, RegistrationForm
 from .models import CustomUser
 from .decorators import role_required
+
+# user login
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not none:
+            login(request, user)
+            message.success(request, "You have successfully logged in!")
+            return redirect('dashboard')
+        else:
+            messages.error(request, "Invalid username or password")
+            
+    return render(request, 'accounts/login.html')
+
+# user logout
+@login_required
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You have been logged out!")
+    return redirect('login')
+
+#
