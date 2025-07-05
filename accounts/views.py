7d5db30455df949876e03f6075fe83aab9f13614
Brian Orandi <brianorandi97@gmail.com>
2025-07-06 01:18:42 +0300

    Added login and logout views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views.generic import CreationView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import CustomUserChangeForm, RegistrationForm
+from .models import CustomUser
+from .decorators import role_required
